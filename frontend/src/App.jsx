b4ceeb0a03d0b1ca5a0fb12fbe330ca3fe5991ce
Srushti-Kamble <kamblesrushti127@gmail.com>
2026-04-25 13:18:36 +0530

    dashboard added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,18 +7,21 @@ import PersonalDetailsPage from "./Pages/PersonalDetailsPage";
 import HomePage from "./Pages/HomePage";
 
 import ShieldHerFlipbook from "./Pages/ShieldHerFlipbook";
-
+import Dashboard from "./Pages/Dashboard";
 function App() {
+  
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<ShieldHerLoader />} />
+        {/* <Route path="/" element={<ShieldHerLoader />} />
 
         <Route path="/login" element={<AuthPages />} />
 
         <Route path="/home" element={<HomePage />} />
 
-        <Route path="/details" element={<PersonalDetailsPage />} />
+        <Route path="/details" element={<PersonalDetailsPage />} /> */}
+
+<Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   );
